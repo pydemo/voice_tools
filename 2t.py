495d0707806355327d26ddb21eb14b8d54659338
alex buz <alex_buz@yahoo.com>
2024-09-28 19:38:59 -0400

    new

---
+++ 2t.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True, cache_dir="cache"
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+# Specify the path to your WAV file
+audio_file = "L1_output.wav"
+
+# Process the audio file
+result = pipe(audio_file)
+print(result["text"])
---
+++ speech2text.py
@@ -0,0 +1,41 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+import click
+
+@click.command()
+@click.option('--in', 'input_file', required=True, type=click.Path(exists=True), help="Path to the input audio file")
+@click.option('--out', 'output_file', required=True, type=str, help="Path to the output text file")
+def main(input_file, output_file):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    model_id = "openai/whisper-large-v3"
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True, cache_dir="cache"
+    )
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        torch_dtype=torch_dtype,
+        device=device,
+        language='en'
+    )
+
+    # Process the input audio file
+    result = pipe(input_file)
+    transcription = result["text"]
+    
+    # Write the transcription to the output file
+    with open(output_file, "w") as f:
+        f.write(transcription)
+    print(f"Transcription saved to {output_file}")
+
+if __name__ == "__main__":
+    main()
